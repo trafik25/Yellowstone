fc32d29c7a2568efc26f3be065d432385b04e29e
Thomas Howard <thomashoward206@gmail.com>
2015-01-07 08:19:17 -0500

    Update LoginPage.cs

--- YellowDemoFramework/Pages/LoginPage.cs
+++ YellowDemoFramework/Pages/LoginPage.cs
@@ -1,9 +1,9 @@
 using System.Runtime.InteropServices;
-using BATDemoFramework.Generators;
+using YellowDemoFramework.Generators;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
-namespace BATDemoFramework
+namespace YellowDemoFramework
 {
     public class LoginPage
     {
